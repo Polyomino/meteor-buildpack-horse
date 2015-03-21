5c999358204c2de56e03ff649943747349edde64
Patryk Ozga <patryk.ozga@outlook.com>
2015-03-20 21:40:34 -0700

    remove dpkg

--- extra/phantomjs.sh
+++ extra/phantomjs.sh
@@ -3,8 +3,6 @@
 VERSION=2.0.0
 PHANTOM_URL="https://s3.amazonaws.com/travis-phantomjs/phantomjs-$VERSION-ubuntu-12.04.tar.bz2"
 
-curl -sS "http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu48_4.8.1.1-3ubuntu0.5_amd64.deb" > libicu.deb
-dpkg -i libicu.deb
 echo "-----> Installing phantomjs."
 
 # Just extract the one file (the binary) and not all the examples/readme's/etc.
