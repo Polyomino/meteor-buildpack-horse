717962b54fde2e05b998057706fe1da749e4ce45
Patryk Ozga <patryk.ozga@outlook.com>
2015-03-20 21:25:59 -0700

    use phantomjs 2.0.0 unoffical

--- extra/phantomjs.sh
+++ extra/phantomjs.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-VERSION=1.9.7
-PHANTOM_URL="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$VERSION-linux-x86_64.tar.bz2"
+VERSION=2.0.0
+PHANTOM_URL="https://s3.amazonaws.com/travis-phantomjs/phantomjs-$VERSION-ubuntu-12.04.tar.bz2"
 
+curl -sS "http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu48_4.8.1.1-3ubuntu0.5_amd64.deb" > libicu.deb
+dpkg -i libicu.deb
 echo "-----> Installing phantomjs."
+
 # Just extract the one file (the binary) and not all the examples/readme's/etc.
-FILE="phantomjs-$VERSION-linux-x86_64/bin/phantomjs"
-curl -L -sS $PHANTOM_URL -o - | tar -jxf - -C $COMPILE_DIR $FILE --strip 1
+FILE="phantomjs"
+curl -L -sS $PHANTOM_URL -o - | tar -jxf - -C $COMPILE_DIR $FILE
